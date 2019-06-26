f04351fdd4ffbb72050584fa9c72b8674bdb3bba
Víctor Andrés Rojas <victandres98@gmail.com>
2019-06-26 12:08:46 -0500

    Initial Config

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -22,13 +22,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// models sequelize setup
+var models = require('./app/models');
+models.sequelize.sync().then(() => {
+  console.log("Se ha conectado a la base de Datos.");
+}).catch(err => {
+  console.log(err, "Ocurrió un error");
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
