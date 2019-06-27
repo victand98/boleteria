6058a964e14802b655f17e71faf76687c2e54808
JhomairaPerez <jhomaira.perez@unl.edu.ec>
2019-06-27 12:57:50 -0500

    controller coop

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -13,6 +13,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'app', 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: 'Quinto B',
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
